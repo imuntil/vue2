144460691df753260723a62b93f9f3148f078f15
imuntil <imuntil@qq.com>
2017-12-27 21:12:24 +0800

    h5-api -mac

--- Nuxt/wd-crm/assets/lib/h5-api.js
+++ Nuxt/wd-crm/assets/lib/h5-api.js
@@ -1,55 +1,50 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext
-// let nfAble
-
-// const audioCtx = new AudioContext()
-// const arrFrenquency = [196.00, 220.00, 246.94, 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50]
-// let start = 0
-// let direction = 1
-// export function run () {
-//   let frequency = arrFrenquency[start]
-//   if (!frequency) {
-//     direction -= -1 * direction
-//     start = start + 2 * direction
-//     frequency = arrFrenquency[start]
-//   }
-//   start = start + direction
-//   const oscillator = audioCtx.createOscillator()
-//   const gainNode = audioCtx.createGain()
-//   oscillator.connect(gainNode)
-//   gainNode.connect(audioCtx.destination)
-//   oscillator.type = 'sine'
-//   oscillator.frequency.value = frequency
-//   gainNode.gain.setValueAtTime(0, audioCtx.currentTime)
-//   gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.01)
-//   oscillator.start(audioCtx.currentTime)
-//   gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 1)
-//   oscillator.stop(audioCtx.currentTime + 1)
-// }
+const arrFrenquency = [196.00, 220.00, 246.94, 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50]
+let start = 0
+let direction = 1
 
 export class AudioCtx {
-  static run () {
-    if (!AudioCtx.audioCtx) return
-    let frequency = AudioCtx.arrFrenquency[AudioCtx.start]
+  constructor () {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext
+    this.audioCtx = new AudioContext()
+  }
+  run () {
+    let frequency = arrFrenquency[start]
     if (!frequency) {
-      AudioCtx.direction -= -1 * AudioCtx.direction
-      AudioCtx.start = AudioCtx.start + 2 * AudioCtx.direction
-      AudioCtx.frequency = AudioCtx.arrFrenquency[AudioCtx.start]
+      direction -= -1 * direction
+      start = start + 2 * direction
+      frequency = arrFrenquency[start]
     }
-    AudioCtx.start = AudioCtx.start + AudioCtx.direction
-    const oscillator = AudioCtx.audioCtx.createOscillator()
-    const gainNode = AudioCtx.audioCtx.createGain()
+    start = start + direction
+    const oscillator = this.audioCtx.createOscillator()
+    const gainNode = this.audioCtx.createGain()
     oscillator.connect(gainNode)
-    gainNode.connect(AudioCtx.audioCtx.destination)
+    gainNode.connect(this.audioCtx.destination)
     oscillator.type = 'sine'
     oscillator.frequency.value = frequency
-    gainNode.gain.setValueAtTime(0, AudioCtx.audioCtx.currentTime)
-    gainNode.gain.linearRampToValueAtTime(1, AudioCtx.audioCtx.currentTime + 0.01)
-    oscillator.start(AudioCtx.audioCtx.currentTime)
-    gainNode.gain.exponentialRampToValueAtTime(0.001, AudioCtx.audioCtx.currentTime + 1)
-    oscillator.stop(AudioCtx.audioCtx.currentTime + 1)
+    gainNode.gain.setValueAtTime(0, this.audioCtx.currentTime)
+    gainNode.gain.linearRampToValueAtTime(1, this.audioCtx.currentTime + 0.01)
+    oscillator.start(this.audioCtx.currentTime)
+    gainNode.gain.exponentialRampToValueAtTime(0.001, this.audioCtx.currentTime + 1)
+    oscillator.stop(this.audioCtx.currentTime + 1)
   }
 }
-AudioCtx.audioCtx = AudioContext ? new AudioContext() : ''
-AudioCtx.arrFrenquency = [196.00, 220.00, 246.94, 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50]
-AudioCtx.start = 0
-AudioCtx.direction = 1
+
+let ntAble = null
+export function notification (msg) {
+  if (ntAble === null) {
+    ntAble = ('Notification' in window)
+  }
+  if (!ntAble) return false
+  if (!('Notification' in window)) return
+  if (Notification.permission === 'granted') {
+    /* eslint-disable no-new */
+    new Notification(msg)
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(permission => {
+      if (permission === 'granted') {
+        /* eslint-disable no-new */
+        new Notification(msg)
+      }
+    })
+  }
+}
